d4851296d9c1f3aab6681fcde12d5a429638e1ad
t-weijiang.qin <t-weijiang.qin@pcitc.com>
2026-03-09 13:55:28 +0800

    添加了线上的用户访问日志

--- apps/pj_bible/app/layout.tsx
+++ apps/pj_bible/app/layout.tsx
@@ -15,11 +15,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className="min-h-screen bg-gray-50 text-gray-900">
-        <Script
-          defer
-          src="http://localhost:3001/script.js"
-          data-website-id="61d289f8-07c4-4868-8115-e0d7966ee619"
-        />
+        <script defer src="http://www.quanyuan.live/umami/script.js" data-website-id="00fe8ddd-43c6-4c19-aa10-4e5f8ac9c8ff"></script>
         <div className="container mx-auto px-4 py-8">
           {children}
         </div>
